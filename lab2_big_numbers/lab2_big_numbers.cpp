4b52283c200c44a176f6c6340a15a74f95337ad7
Дошик Терешков < @DESKTOP-FGI2GNG>
2021-10-18 23:39:14 +0300

    add karatsuba multiplication

--- lab2_big_numbers/lab2_big_numbers.cpp
+++ lab2_big_numbers/lab2_big_numbers.cpp
@@ -6,6 +6,11 @@
 class BigNumber {
 public:
     BigNumber(std::string s) {
+        if (s.length() == 0) {
+            std::cout << "Empty string!\n";
+            std::exit(1);
+        }
+
         this->data.resize(s.length(), 0);
         for (int i = 0; i < s.length(); ++i) {
             auto n = cifers.find_first_of(s[i]);
@@ -22,6 +27,10 @@ public:
     };
 
     BigNumber(std::vector<int> v) {
+        if (v.size() == 0) {
+            std::cout << "Empty vector!\n";
+            std::exit(1);
+        }
         this->data = v;
 
         remove_zeros_from_start();
@@ -74,7 +83,7 @@ public:
         return BigNumber(new_data);
     };
 
-    BigNumber sub(const BigNumber& other) {
+    BigNumber sub(const BigNumber &other) {
         std::vector<int> new_data;
         int carry = 0;
         int first, second;
@@ -148,6 +157,44 @@ public:
         return BigNumber(new_data);
     }
 
+    BigNumber mul_karatsuba(BigNumber& other) {
+        // https://en.wikipedia.org/wiki/Karatsuba_algorithm
+        int my_size = this->data.size();
+        
+        if (my_size < 2) {
+            return other.multiply(this->data[0]);
+        }
+
+        int other_size = other.data.size();
+
+        if (other_size < 2) {
+            return this->multiply(other.data[0]);
+        }
+        
+        int splitter = std::min(my_size, other_size) / 2;
+
+        BigNumber high1 = this->cut(my_size - 1, splitter);
+        BigNumber low1 = this->cut(splitter - 1, 0);
+
+        BigNumber high2 = other.cut(other_size - 1, splitter);
+        BigNumber low2 = other.cut(splitter - 1, 0);
+
+        BigNumber z0 = low1.mul_karatsuba(low2);
+
+        BigNumber z1_1 = low1.add(high1);
+        BigNumber z1_2 = low2.add(high2);
+        BigNumber z1 = z1_1.mul_karatsuba(z1_2);
+        
+        BigNumber z2 = high1.mul_karatsuba(high2);
+
+        BigNumber result_1 = z2.shift_to_left(splitter * 2);
+        BigNumber result_2 = result_1.add(z0);
+        BigNumber result_3 = result_2.add(z1.shift_to_left(splitter));
+        BigNumber result_4 = z2.add(z0).shift_to_left(splitter);
+
+        return result_3.sub(result_4);
+    }
+
 private:
     std::vector<int> data;
     const std::string cifers = "0123456789";
@@ -168,11 +215,6 @@ int main()
 
     BigNumber b(s);
 
-    std::cout << a.to_string() << "\n";
+    std::cout << "Karatsuba multiplication: " << a.to_string() << " * " << b.to_string() << " = " << a.mul_karatsuba(b).to_string() << "\n";
 
-    BigNumber c = a.shift_to_left(3);
-    std::cout << c.to_string() << "\n";
-
-    std::cout << a.cut(5, 3).to_string() << "\n";
-    std::cout << a.cut(2, 0).to_string() << "\n";
 }
