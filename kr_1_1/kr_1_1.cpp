62daefb678dd94fc3045db36faea10301684236b
Дошик Терешков < @DESKTOP-FGI2GNG>
2021-10-23 12:39:22 +0300

    compete sr 1_1 for variant 8

---
+++ kr_1_1/kr_1_1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::cout.precision(30);
+	double result = 0.5;
+	double sn = 0.5;
+	for (int n = 2; n < 31; n++) {
+		sn *= (-1) * 0.25 / n * (2 * n - 3) / (2 * n - 1);
+		result += sn;
+		
+		
+		if (n > 20)
+			std::cout << result<< "\n";
+	}
+
+}
+
+
