9dfc03c69e7c9fe2a92bf01c2dcb875aa0c0f012
Дошик Терешков < @DESKTOP-FGI2GNG>
2021-10-28 23:15:01 +0300

    boolean operations and sign support

--- lab2_big_numbers/lab2_big_numbers.cpp
+++ lab2_big_numbers/lab2_big_numbers.cpp
@@ -8,13 +8,25 @@ class BigNumber {
 public:
     BigNumber(std::string s) {
 
-        if (s.length() == 0) {
-            std::cout << "Empty string!\n";
+        this->sign = +1;
+        this->data.resize(s.length(), 0);
+        int numbert_starts_at = 0;
+
+        if (s[0] == '-') {
+            this->sign = -1;
+            numbert_starts_at = 1;
+        }
+
+        if (s[0] == '+') {
+            numbert_starts_at = 1;
+        }
+
+        if (numbert_starts_at == s.length()) {
+            std::cout << s << " is not number!\n";
             std::exit(1);
         }
 
-        this->data.resize(s.length(), 0);
-        for (size_t i = 0; i < s.length(); ++i) {
+        for (size_t i = numbert_starts_at; i < s.length(); ++i) {
             auto n = cifers.find_first_of(s[i]);
 
             if (n == std::string::npos) {
@@ -34,6 +46,9 @@ public:
 
     BigNumber() {
         this->data.clear();
+        this->data.push_back(0);
+        this->sign = +1;
+        this->id = 0;
         if (this->is_debug) {
             this->id = rand();
             std::cout << "Empty constructor " << this->id << " " << this->to_string() << "\n";
@@ -47,7 +62,15 @@ public:
 
     BigNumber operator *(int x) {
         BigNumber output;
+        output.data.clear();
         int carry = 0;
+
+        int other_sign = +1;
+        if (x < 0)
+            other_sign = -1;
+
+        x = abs(x);
+
 
         for (auto it = this->data.begin(); it != this->data.end(); ++it) {
             int result = *it * x + carry;
@@ -59,20 +82,23 @@ public:
         if (carry != 0)
             output.data.push_back(carry);
 
+
+        output.sign = this->sign * other_sign;
         output.remove_zeros_from_start();
 
         return output;
     };
 
+    // TODO sign
     BigNumber operator +(const BigNumber &other) {
         BigNumber output;
         int carry = 0;
         int first, second;
-        int max_size = std::max(this->data.size(), other.data.size()) + 1;
+        size_t max_size = std::max(this->data.size(), other.data.size()) + 1;
 
         output.data.resize(max_size, 0);
 
-        for (int i = 0; i < max_size; i++) {
+        for (size_t i = 0; i < max_size; i++) {
             if (i < this->data.size())
                 first = this->data[i]; 
             else 
@@ -95,15 +121,16 @@ public:
         return output;
     };
 
+    // TODO sign
     BigNumber operator -(const BigNumber &other) {
         BigNumber output;
         int carry = 0;
         int first, second;
-        int max_size = std::max(this->data.size(), other.data.size()) + 1;
+        size_t max_size = std::max(this->data.size(), other.data.size()) + 1;
 
         output.data.resize(max_size, 0);
 
-        for (int i = 0; i < max_size; i++) {
+        for (size_t i = 0; i < max_size; i++) {
             if (i < this->data.size())
                 first = this->data[i];
             else
@@ -134,6 +161,9 @@ public:
 
     std::string to_string() {
         std::string result;
+
+        if (this->sign < 0)
+            result.push_back('-');
         
         if (this->data.size())
             for (auto it = this->data.rbegin(); it != this->data.rend(); ++it)
@@ -156,6 +186,14 @@ public:
         return output;
     }
 
+    bool operator ==(BigNumber& other) {
+            return this->to_string() == other.to_string();
+    }
+
+    bool operator !=(BigNumber& other) {
+        return this->to_string() != other.to_string();
+    }
+
     BigNumber operator <<(int x) {
         BigNumber output;
         output.data.resize(this->data.size() + x, 0);
@@ -167,8 +205,77 @@ public:
         return output;
     }
 
+    BigNumber operator +() {
+        BigNumber output;
+        output.sign = this->sign;
+        output.data = this->data;
+        output.remove_zeros_from_start();
+        return output;
+    }
+
+    BigNumber operator -() {
+        BigNumber output;
+        output.sign = -this->sign;
+        output.data = this->data;
+        output.remove_zeros_from_start();
+        return output;
+    }
+
+    bool operator < (BigNumber& other) {
+        if (*this == other)
+            return false;
+
+        if (this->sign < 0 and other.sign >0)
+            return true;
+
+        if (this->sign > 0 and other.sign <0)
+            return false;
+
+
+        if (this->sign < 0 and other.sign < 0) {
+            BigNumber not_this = -(*this);
+            BigNumber not_other = -other;
+            return  not_other < not_this;
+        }
+            
+
+
+        if (this->data.size() < other.data.size())
+            return true;
+
+        if (this->data.size() > other.data.size())
+            return false;
+
+        for (int i = other.data.size() - 1; i >= 0; i--) {
+            if (this->data[i] == other.data[i])
+                continue;
+
+            return this->data[i] < other.data[i];
+        }
+
+        return true;
+    }
+
+
+    bool operator <= (BigNumber& other)  {
+        return (*this < other || ((*this) == other));
+    }
+
+    bool operator >= (BigNumber& other)  {
+        return !(*this < other);
+    }
+
+    bool operator > (BigNumber& other)  {
+        return !(*this <= other);
+    }
+
     BigNumber mul_karatsuba(BigNumber& other) {
         // https://en.wikipedia.org/wiki/Karatsuba_algorithm
+        if (this->sign < 0 or other.sign < 0) {
+            std::cout << "mul_karatsuba() works only with positive numbers!\n";
+            std::exit(1);
+        }
+
         int my_size = this->data.size();
         
         if (my_size < 2) {
@@ -216,12 +323,17 @@ public:
         result.remove_zeros_from_start();
         return result;
     }
+
 private:
     void remove_zeros_from_start() {
         while (this->data[this->data.size() - 1] == 0 and this->data.size() > 1)
             this->data.pop_back();
+
+        if (this->data.size() == 1 and this->data[0] == 0)
+            this->sign = +1;
     };
 
+    int sign;
     std::vector<int> data;
     const std::string cifers = "0123456789";
     int id;
@@ -231,6 +343,100 @@ private:
 
 int main()
 {
+    BigNumber a("-1");
+    BigNumber b("-1");
+    std::cout << "a != b:" << (a != b) << " \n";
+    std::cout << "a == b:" << (a == b) << " \n";
+
+    BigNumber c("-5");
+    BigNumber d("-2");
+    BigNumber e("0");
+    BigNumber f("-0");
+    BigNumber g("2");
+    BigNumber h("1");
+    BigNumber i("5432");
+    std::cout << "a: " << a.to_string() << " \n";
+    std::cout << "b: " << b.to_string() << " \n";
+    std::cout << "c: " << c.to_string() << " \n";
+    std::cout << "d: " << d.to_string() << " \n";
+    std::cout << "e: " << e.to_string() << " \n";
+    std::cout << "f: " << f.to_string() << " \n";
+    std::cout << "g: " << g.to_string() << " \n";
+    std::cout << "h: " << h.to_string() << " \n";
+    std::cout << "i: " << i.to_string() << " \n";
+
+    std::cout << "i(3, 2) .. i(1,0): " << i(3, 2).to_string() << " .. " << i(1, 0).to_string() << " \n";
+
+    std::cout << "a*2: " << (a * 2).to_string() << " \n";
+    std::cout << "b*2: " << (b * 2).to_string() << " \n";
+    std::cout << "c*2: " << (c * 2).to_string() << " \n";
+    std::cout << "d*2: " << (d * 2).to_string() << " \n";
+    std::cout << "e*2: " << (e * 2).to_string() << " \n";
+    std::cout << "f*2: " << (f * 2).to_string() << " \n";
+    std::cout << "g*2: " << (g * 2).to_string() << " \n";
+
+    std::cout << "a*0: " << (a * 0).to_string() << " \n";
+    std::cout << "b*0: " << (b * 0).to_string() << " \n";
+    std::cout << "c*0: " << (c * 0).to_string() << " \n";
+    std::cout << "d*0: " << (d * 0).to_string() << " \n";
+    std::cout << "e*0: " << (e * 0).to_string() << " \n";
+    std::cout << "f*0: " << (f * 0).to_string() << " \n";
+    std::cout << "g*0: " << (g * 0).to_string() << " \n";
+
+    std::cout << "a*-2: " << (a * -2).to_string() << " \n";
+    std::cout << "b*-2: " << (b * -2).to_string() << " \n";
+    std::cout << "c*-2: " << (c * -2).to_string() << " \n";
+    std::cout << "d*-2: " << (d * -2).to_string() << " \n";
+    std::cout << "e*-2: " << (e * -2).to_string() << " \n";
+    std::cout << "f*-2: " << (f * -2).to_string() << " \n";
+    std::cout << "g*-2: " << (g * -2).to_string() << " \n";
+
+    std::cout << "+a: " << (+a).to_string() << " \n";
+    std::cout << "+b: " << (+b).to_string() << " \n";
+    std::cout << "+c: " << (+c).to_string() << " \n";
+    std::cout << "+d: " << (+d).to_string() << " \n";
+    std::cout << "+e: " << (+e).to_string() << " \n";
+    std::cout << "+f: " << (+f).to_string() << " \n";
+    std::cout << "+g: " << (+g).to_string() << " \n";
+    std::cout << "+h: " << (+h).to_string() << " \n";
+    std::cout << "+i: " << (+i).to_string() << " \n";
+
+    std::cout << "-a: " << (-a).to_string() << " \n";
+    std::cout << "-b: " << (-b).to_string() << " \n";
+    std::cout << "-c: " << (-c).to_string() << " \n";
+    std::cout << "-d: " << (-d).to_string() << " \n";
+    std::cout << "-e: " << (-e).to_string() << " \n";
+    std::cout << "-f: " << (-f).to_string() << " \n";
+    std::cout << "-g: " << (-g).to_string() << " \n";
+    std::cout << "-h: " << (-h).to_string() << " \n";
+    std::cout << "-i: " << (-i).to_string() << " \n";
+
+    std::cout << "a < i: " << (a < i) << " \n";
+    std::cout << "i < a: " << (i < a) << " \n";
+    std::cout << "i < i: " << (i < i) << " \n";
+    std::cout << "a < a: " << (a < a) << " \n";
+
+
+    std::cout << "e: " << e.to_string() << " \n";
+    std::cout << "g: " << g.to_string() << " \n";
+    std::cout << "i: " << i.to_string() << " \n";
+
+    std::cout << "e < g: " << (e < g) << " \n";
+    std::cout << "e < i: " << (e < i) << " \n";
+    std::cout << "g < e: " << (g < e) << " \n";
+    std::cout << "g < i: " << (g < i) << " \n";
+    std::cout << "i < e: " << (i < e) << " \n";
+    std::cout << "i < g: " << (i < g) << " \n";
+
+    std::cout << "c: " << c.to_string() << " \n";
+    std::cout << "d: " << d.to_string() << " \n";
+    
+    std::cout << "c < d: " << (c < d) << " \n";
+    std::cout << "d < c: " << (d < c) << " \n";
+
+
+
+    /*
     std::string s;
 
     std::cout << "First: ";
@@ -243,4 +449,5 @@ int main()
 
     std::cout << "Karatsuba multiplication: " << a.to_string() << " * " << b.to_string() << " = " << a.mul_karatsuba(b).to_string() << "\n";
     std::cout << "Schonhage-Strassen multiplication: " << a.to_string() << " * " << b.to_string() << " = " << a.mul_schonhage(b).to_string() << "\n";
+    */
 }
